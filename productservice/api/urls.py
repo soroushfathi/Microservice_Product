4c3644d57cc177693ccf8b2329bcec90ebe224ed
soroushfathi <soroush8fathi@gmail.com>
2024-06-14 01:20:42 +0330

    add auth apis

--- productservice/api/urls.py
+++ productservice/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('users/', include(('productservice.users.urls', 'users'))),
+    path('auth/', include(('productservice.authentication.urls', 'auth'))),
     path('products/', include(('productservice.product.urls', 'products')))
 ]
