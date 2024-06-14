a7db18f627955592f04b5725ae8fecb4e11c707e
soroushfathi <soroush8fathi@gmail.com>
2024-06-14 16:58:22 +0330

    comment extra line

--- productservice/product/urls.py
+++ productservice/product/urls.py
@@ -5,8 +5,8 @@ from .views import (
 urlpatterns = [
     path('list/', ProductListAPIView.as_view(), name='product-list'),
     path('<int:id>/', ProductDetailAPIView.as_view(), name='product-detail'),
-    path('<int:id>/update/', ProductDetailAPIView.as_view(), name='product-update'),
-    path('<int:id>/delete/', ProductDetailAPIView.as_view(), name='product-delete'),
+    # path('<int:id>/update/', ProductDetailAPIView.as_view(), name='product-update'),
+    # path('<int:id>/delete/', ProductDetailAPIView.as_view(), name='product-delete'),
     path('', ProductCreateAPIView.as_view(), name='product-create'),
 ]
 
