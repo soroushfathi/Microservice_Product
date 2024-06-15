5f159d30df027bc96273c331d0cf0aa73ef50be6
soroushfathi <soroush8fathi@gmail.com>
2024-06-15 22:19:35 +0330

    update path due to the new docker-compose

--- docker/web_entrypoint.sh
+++ docker/web_entrypoint.sh
@@ -5,11 +5,12 @@ echo "--> Waiting for db to be ready"
 
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py makemigration
-python manage.py migrate
-python manage.py collectstatic --clear --noinput
-python manage.py collectstatic --noinput
+python ./ProductService/manage.py makemigrations
+python ./ProductService/manage.py migrate
+python ./ProductService/manage.py collectstatic --clear --noinput
+python ./ProductService/manage.py collectstatic --noinput
 
 # Start server
 echo "--> Starting web process"
+cd ProductService
 gunicorn config.wsgi:application -b 0.0.0.0:8000
